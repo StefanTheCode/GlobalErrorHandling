4cd834a4dcdba43746fcad5e6b02afd50f946674
stefan.djokic <stefan.djokic@exlrt.com>
2023-05-29 10:27:38 +0200

    Done

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace GlobalErrorHandling.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            Console.WriteLine("Error: " + error.Message);
+            Console.WriteLine("Error Details: " + context.Request.Path);
+
+            if (error.InnerException != null)
+                Console.WriteLine("Inner Error: " + error.InnerException.Message);
+
+            //Return StatusCode, Message, Details as result
+            await response.WriteAsync("Error");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+using GlobalErrorHandling.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
